5169456edba3ad518046fe893c6d2a9a0a963a01
Israel Cabrera <73441062+IsraelCabreraPortilla@users.noreply.github.com>
2022-07-14 13:43:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,flash
 import pandas as pd
 import requests
 from urllib.request import urlopen
@@ -7,6 +7,7 @@ from datetime import datetime
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import numpy as np
 import time
 
 UPLOAD_FOLDER = "./static"
@@ -15,20 +16,26 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # TOKEN Quant Intern 1
 token1 = "5363622455:AAEoFyCSHrhOZhQzxSGD6MxOWKw9h9aoQqY"
-lista_enviados = []
+
 ids = []
 array = []
-
+lista_enviados = np.array([])
+ids = np.array([])
+lista_con_info = []
+cont = 1
 
 # function for sending just a message
 def envio_msj(id_chat, msg):
+    global lista_enviados,ids,lista_con_info,cont
     r = requests.post(f'https://api.telegram.org/bot{token1}/sendMessage',
                       data={'chat_id': id_chat, 'text': msg})
     if(r.json()['ok']):
-        lista_enviados.append(r.json()['result']['chat']['title'])
-        ids.append(r.json()['result']['chat']['id'])
-    time.sleep(0.01)    
-    
+        lista_enviados = np.append(lista_enviados, r.json()['result']['chat']['title'])
+        ids = np.append(ids, r.json()['result']['chat']['id'])
+        lista_con_info.append(f"\nSe mandó a {str(lista_enviados[-1])}, ya van {str(cont)}.")
+        cont += 1
+        #print(lista_enviados)
+        
  
 
 
@@ -38,11 +45,12 @@ def envio_msjIMG(id_chat, msg, imagen):
                       files={'photo': (imagen, open(
                           imagen, 'rb'))},
                       data={'chat_id': id_chat, 'caption': msg})
+    time.sleep(0.001)
    
 
 @app.route('/', methods=["get", "post"])
 def upload_file():
-    lista_con_info = []
+    
     # defining the scope of the application
     scope_app =['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'] 
 
@@ -51,16 +59,17 @@ def upload_file():
         # authorize the clientsheet 
     client = gspread.authorize(cred)
         # get the sample of the Spreadsheet
-    #sheet = client.open('Telegram_Chats')
+        #sheet = client.open('Telegram_Chats')
     sheet_pruebas = client.open('Client_List')
 
         # get the first sheet of the Spreadsheet
-    #sheet_instance = sheet.get_worksheet(0)
+        #sheet_instance = sheet.get_worksheet(0)
     sheet_instancepruebas = sheet_pruebas.get_worksheet(0)
-    # get all the records of the data
-    #records_oficial = sheet_instance.get_all_records()
+        # get all the records of the data
+        #records_oficial = sheet_instance.get_all_records()
     records_pruebas = sheet_instancepruebas.get_all_records()
     data = records_pruebas
+    print(len(data))
         
     my_list = []
 
@@ -108,6 +117,7 @@ def upload_file():
                 if(region == "All" and category=="All"):
                     for i in range(len(data)):
                         envio_msj(data[i]['ID_Chat'], message)
+                        print(i)
                 if(region =="All" and category!="All"):
                     for i in range(len(data)):
                         if(category == data[i]['Category']):
@@ -121,22 +131,9 @@ def upload_file():
                         if(region == data[i]['Site'] and category == data[i]['Category']):
                             envio_msj(data[i]['ID_Chat'],message)
         
+            
 
-
-            for i in range(len(lista_enviados)):
-                lista_con_info.append(f"\nSe mandó a {str(lista_enviados[i])}, ya van {str(cont)}.")
-                cont += 1
-                diccionario = {
-                'Name Group': lista_enviados[i],
-                'id':ids[i]
-                }
-                
-
-                array.append(diccionario)
-
-            df = pd.DataFrame(array)
-            df.to_excel("Registro_Enviados.xlsx")
-            print('Successfully exported into Excel File')
+   
 
         except Exception as e: 
             lista_con_info.append(e)
